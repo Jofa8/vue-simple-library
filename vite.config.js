4f6320fc631a6d5897ba37f537dbaf7471f892e9
Jofa8 <106910992+Jofa8@users.noreply.github.com>
2023-08-16 09:42:59 +0200

    vite.conf.js correction

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
     base: '/vue-simple-library/',
+    mode,
+    publicDir: "../public",
+    build: {
+        outDir: "../dist",
+        assetsDir: "./"
+    }
 })
